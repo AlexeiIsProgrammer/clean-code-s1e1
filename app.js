686eef7c203a1fe223bd66cd6eaf51c0c40ee1a8
Alexei <avenge.now.r@mail.ru>
2023-04-19 12:04:34 +0300

    fix: change name from 'task-ite' to 'task-item'

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var createNewTaskElement = function (taskString) {
 
 
     //and appending.
-    listItem.className = "task-ite"
+    listItem.className = "task-item"
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
     listItem.appendChild(editInput);
